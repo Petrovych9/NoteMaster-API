768c496beefdb7b00d7b997657dafcc3e7d00604
Bohdan <bohdan.work@urk.net>
2023-09-28 15:33:26 +0300

    add note model

--- app/main.py
+++ app/main.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI
+from models.notes import Note
 
 app = FastAPI(
     title='NoteMaster-API'
 )
 
 
-@app.get('/home')
-async def home():
-    return 'Home page'
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
 
+@app.post("/notes/")
+async def create_note(note: Note):
+    return note
+
+
+@app.get("/notes/")
+async def show_all_notes():
+    notes = {}
+    return notes
