1f5efb141ef46fad215657d83a8786c67037199f
name <iam@example.com>
2023-11-20 17:18:30 +0200

    Added basic models for db in models.py file

---
+++ app/models.py
@@ -0,0 +1,52 @@
+from datetime import datetime
+
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Enum
+from sqlalchemy.orm import Session
+from sqlalchemy.ext.declarative import declarative_base
+
+from app.config import DATABASE_URL
+
+Base = declarative_base()
+
+
+def connect_db():
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={}
+    )
+    session = Session(bind=engine.connect())
+    return session
+
+
+class NoteStatus(Enum):
+    personal = 'personal'
+    work = 'work'
+    ideas = 'ideas'
+    shopping_list = 'shopping list'
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer),
+    email = Column(String)
+    password = Column(String)
+    nickname = Column(String)
+    created_at = Column(String, default=datetime.utcnow())
+
+
+class Note(Base):
+    __tablename__ = 'notes'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    title = Column(String)
+    content = Column(String)
+    category = Column(String)
+    status = Column(String, default=NoteStatus.personal)
+
+
+class AuthToken(Base):
+    __tablename__ = 'auth_token'
+    id = Column(Integer, primary_key=True)
+    token = Column(String)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    created_at = Column(String, default=datetime.utcnow())
