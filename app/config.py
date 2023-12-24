c7efaca67c810e3d5d9e15cae72ff048ac869a28
name <iam@example.com>
2023-12-24 12:12:55 +0200

    add class settings

--- app/config.py
+++ app/config.py
@@ -1,6 +1,7 @@
 import os
 from starlette.config import Config     # todo what is it
 from starlette.datastructures import CommaSeparatedStrings, Secret     # todo what is it
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 root_dir = dir_path[:-3]
@@ -11,4 +12,8 @@ DATABASE_URL_TEST = f"sqlite:///{root_dir}" + config('TEST_DB_NAME', cast=str)
 
 SECRET_KEY = config('SECRET_KEY', cast=Secret)
 
-# TODO add class settings
+
+class Settings(BaseSettings):
+    app_name: str = 'NoteMaster-API'
+    # model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
+
--- app/main.py
+++ app/main.py
@@ -1,13 +1,23 @@
-from fastapi import FastAPI
+from functools import lru_cache
+
+from fastapi import FastAPI, Depends
+from typing_extensions import Annotated
+
+from app.config import Settings
 from app.handlers import auth_router
 
 
-def get_app() -> FastAPI:
+@lru_cache
+def get_settings():
+    return Settings()
+
+
+def get_app(settings:  Annotated[Settings, Depends(get_settings)]) -> FastAPI:
     app = FastAPI(
-        title='NoteMaster-API'
+        title=settings.app_name
     )
     app.include_router(auth_router)
     return app
 
 
-app = get_app()
+app = get_app(get_settings())
