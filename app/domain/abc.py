38a37981aacb511967f41d38fd4a09e0ba965dae
name <iam@example.com>
2024-01-07 18:40:53 +0200

    feat: integrate session manager and test cases into abc crud layer

--- app/domain/abc.py
+++ app/domain/abc.py
@@ -7,7 +7,7 @@ from sqlalchemy.exc import IntegrityError, DataError, DBAPIError
 
 from app.models import Base
 
-from app.db import get_db_session
+from app.db import DatabaseSessionManager
 from app.models import User, Note, AuthToken
 
 
@@ -37,31 +37,34 @@ class ABCCrud(ABC):
 class DatabaseCrud(ABCCrud):
     table: Type[Base] = None
 
+    def __init__(self, tests: bool = False):
+        self.tests = tests
+
     def get(self, field_value: dict):
-        with get_db_session() as session:
+        with DatabaseSessionManager(self.tests) as session:
             q = select(self.table).filter_by(**field_value)
             res = session.execute(q)
             return res.scalar()
 
     def create(self, data: dict):
         try:
-            with get_db_session() as session:
+            with DatabaseSessionManager(self.tests) as session:
                 item = self.table(**data)
                 session.add(item)
                 session.commit()
                 return item.id
 
         except IntegrityError as e:
-            raise f"IntegrityError: {e}"
+            raise IntegrityError(f"IntegrityError: {e}")
         except DataError as e:
-            raise f"DataError: issue with the data you are trying to insert |  {e}"
+            raise DataError(f"DataError: issue with the data you are trying to insert |  {e}")
         except DBAPIError as e:
-            raise f"DBAPIError: raised for issues such as connection problems | {e}"
+            raise DBAPIError(f"DBAPIError: raised for issues such as connection problems | {e}")
         except Exception as e:
-            raise f"Unexpected error: {e}"
+            raise Exception(f"Unexpected error: {e}")
 
     def update(self, item_id: int, field_value: dict):
-        with get_db_session() as session:
+        with DatabaseSessionManager(self.tests) as session:
             q = update(self.table).filter_by(id=item_id).values(**field_value).returning(self.table.id)
             res = session.execute(q)
             res = res.fetchone()
@@ -70,7 +73,7 @@ class DatabaseCrud(ABCCrud):
             return res
 
     def delete(self, item_id: int):
-        with get_db_session() as session:
+        with DatabaseSessionManager(self.tests) as session:
             q = delete(self.table).filter_by(id=item_id)
             res = session.execute(q)
             session.commit()
